cff37415daddb1375fa33e4b1182f640ca7d2ea2
KillzoneKid <killzone_kid@hotmail.co.uk>
2018-03-09 21:12:45 +0000

    rewritten

--- strtok.cpp
+++ strtok.cpp
@@ -1,7 +1,7 @@
 /*
 	Split string with delimiters, similar to strtok
 	Will not return empty strings
-	https://ideone.com/apTyNv
+	https://ideone.com/LJota9
 */
 
 #include <iostream> 
@@ -9,11 +9,15 @@
 
 int main()
 {
-	std::string str = "onion ,  'potato .tomato. ' cucumber' .";
-	
-	std::string const delims{" ,.'"};
-	for (size_t pos, beg = 0; (pos = str.find_first_of(delims, beg)) != str.npos; beg = str.find_first_not_of(delims, pos + 1))
+	std::string str = "Hello, everyone! This is: COSC-1436, SP18";
+	std::string const delims{ " .,:;!?" };
+
+	size_t beg, pos = 0;
+	while ((beg = str.find_first_not_of(delims, pos)) != std::string::npos)
+	{
+		pos = str.find_first_of(delims, beg + 1);
 		std::cout << str.substr(beg, pos - beg) << std::endl;
+	}
 	
 	return 0;
 }
