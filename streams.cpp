85b39d2d03f117d440c1c96198102b44fe8e0a58
KillzoneKid <killzone_kid@hotmail.co.uk>
2018-03-11 12:52:31 +0000

    fun with streams

---
+++ streams.cpp
@@ -0,0 +1,34 @@
+/*
+	Fun with streams
+	1. Reading whole file into a string
+	2. Reading whole file into std::cin
+*/
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iterator>
+
+int main()
+{
+	// open file
+	std::ifstream ifs("C:\\Users\\KK\\Desktop\\test.txt");
+
+	// read the whole file into std::ostringstream and print
+	std::ostringstream oss;
+	oss << ifs.rdbuf();
+	std::cout << oss.str() << std::endl;
+
+	// rewind the file and reset flags
+	ifs.seekg(0);
+	ifs.clear();
+
+	// read the whole file into std::cin
+	std::cin.rdbuf(ifs.rdbuf());
+	
+	// iterate through data and print
+	std::istream_iterator<std::string> it(std::cin);
+	std::istream_iterator<std::string> eos;
+	for (; it != eos; ++it)
+		std::cout << *it << std::endl;
+}
