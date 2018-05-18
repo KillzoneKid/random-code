ca86256811502ea8b69929e2a375956511d4ba44
KillzoneKid <killzone_kid@hotmail.co.uk>
2018-05-18 21:00:45 +0100

    like a pro

--- setconsolewindow.cpp
+++ setconsolewindow.cpp
@@ -12,8 +12,7 @@ void SetConsoleWindow(SHORT cols, SHORT rows)
 	CONSOLE_SCREEN_BUFFER_INFOEX sbInfoEx;
 	sbInfoEx.cbSize = sizeof(CONSOLE_SCREEN_BUFFER_INFOEX);
 	GetConsoleScreenBufferInfoEx(conout, &sbInfoEx);
-	sbInfoEx.dwSize.X = cols;
-	sbInfoEx.dwSize.Y = rows;
+	sbInfoEx.dwSize = { cols, rows };
 	sbInfoEx.srWindow = { 0, 0, cols, rows };
 	sbInfoEx.dwMaximumWindowSize = { cols, rows };
 	SetConsoleScreenBufferInfoEx(conout, &sbInfoEx);
@@ -32,16 +31,18 @@ int main()
 	SHORT cols = 100, rows = 60;
 	SetConsoleWindow(cols, rows);
 
-	for (int y = 0; y < rows; ++y)
+	for (int y = 1; y <= rows; ++y)
 	{
 		for (int x = 0; x < cols; ++x)
-			std::cout << '.';
-		
-		if (y < rows - 1)
+			std::cout << x % 10;
+
+		if (y < rows)
 			std::cout << std::endl;
 	}
 
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,0 });
+	//SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 });
+
+	std::cin.get();
 
 	return 0;
 }
